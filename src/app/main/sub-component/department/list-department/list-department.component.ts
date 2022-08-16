624fc3634634f9a2dba8f3318fefc069ed42ce7d
atsawinana <atsawin443@hotmail.com>
2022-08-16 20:22:24 +0700

    list-department
    Created at : 14/08/2565
    Update at : 16/08/2565
    Author : Atsawin Anantasri
    Detail : html layout on list department page

--- src/app/main/sub-component/department/list-department/list-department.component.ts
+++ src/app/main/sub-component/department/list-department/list-department.component.ts
@@ -8,18 +8,19 @@ import { ListDepartmentService } from './list-department.service';
 })
 export class ListDepartmentComponent implements OnInit {
 
+  data: any
+
   constructor(private DepService: ListDepartmentService) { }
 
   ngOnInit() {
     this.DepService.getAllDepartment().subscribe({
       next: (res: any) => {
-
-        console.log('ชื่อ', res.status)
-        res.data.dept_name_en
-
+        this.data = res.data
+        // console.log("api success !")
+        console.log(this.data)
       },
-      error: (err) => {
-        console.log("api failed !")
+      error: (err: any) => {
+        // console.log("api failed !")
       },
     });
   }
