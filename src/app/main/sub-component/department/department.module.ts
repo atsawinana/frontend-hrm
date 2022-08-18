971e648b8c6b9f0731bbbfa828d6da4a6177cfae
atsawinana <atsawin443@hotmail.com>
2022-08-18 22:51:56 +0700

    list department page
    Created at : 14/08/2565
    Update at : 18/08/2565
    Author : Atsawin Anantasri
    Detail : search bar (only text) can search name of department

--- src/app/main/sub-component/department/department.module.ts
+++ src/app/main/sub-component/department/department.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddDepartmentComponent } from './add-department/add-department.component';
 import { ListDepartmentComponent } from './list-department/list-department.component';
 import { DepartmentRoutes } from './department.routing';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   imports: [
     CommonModule,
-    DepartmentRoutes
+    DepartmentRoutes,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   declarations: [
     AddDepartmentComponent,
--- src/app/main/sub-component/department/list-department/list-department.component.ts
+++ src/app/main/sub-component/department/list-department/list-department.component.ts
@@ -4,25 +4,40 @@ import { ListDepartmentService } from './list-department.service';
 @Component({
   selector: 'app-list-department',
   templateUrl: './list-department.component.html',
-  styleUrls: ['./list-department.component.css']
+  styleUrls: ['./list-department.component.css'],
 })
 export class ListDepartmentComponent implements OnInit {
-
-  data: any
-
-  constructor(private DepService: ListDepartmentService) { }
+  data: any 
+  searchInput: any
+  depName?: string
+  depNum?: string
+  depMember?: string
+  constructor(private DepService: ListDepartmentService) {}
 
   ngOnInit() {
     this.DepService.getAllDepartment().subscribe({
       next: (res: any) => {
-        this.data = res.data
-        // console.log("api success !")
-        console.log(this.data)
+        this.data  = res.data;
+        this.depNum = this.data.dept_id
+        this.depName = this.data.dept_name_en
+        this.depMember = this.data.number
       },
-      error: (err: any) => {
-        // console.log("api failed !")
-      },
+      error: (err: any) => {},
     });
+
+  }
+
+  numberOnly(event: { which: any; keyCode: any; }): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      console.log('event.which',event.which)
+      console.log('event.keyCode',event.keyCode)
+      return true;
+    }
+    console.log('event.which',event.which)
+    console.log('event.keyCode',event.keyCode)
+    return false;
+
   }
 
 }
