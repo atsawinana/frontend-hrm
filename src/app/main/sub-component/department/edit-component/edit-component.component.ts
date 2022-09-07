4f9d3642a274c99b4e6fb2868a6fb292da1d82d0
tanathonch <63160090@go.buu.ac.th>
2022-09-07 11:23:01 +0700

    edit-department
    Created at : 22/08/2565
    Update at : 7/09/2565
    Author : Tanathon Chalowthammaporn
    Detail : Update delete input field

--- src/app/main/sub-component/department/edit-component/edit-component.component.ts
+++ src/app/main/sub-component/department/edit-component/edit-component.component.ts
@@ -10,10 +10,38 @@ export class EditComponentComponent implements OnInit {
 
   constructor() { }
 
+  countDept:any = 0
+  countBoss:number = 0
+
   ngOnInit() {
   }
 
-  addDept() {
-    const add = document.createElement('input')
+  getOnlyElement(){
+    let elem = document.getElementsByClassName("iconDept")[this.countBoss]
+    elem.classList.add('showDisplay');
+    // elem.addEventListener('click',this.deleteDept()!)
+
   }
+
+  addDept() {
+     const add = document.getElementById("deptOrigin")!
+     const clone = add.cloneNode(true)
+     document.getElementById("dept")?.appendChild(clone) 
+     this.countBoss++
+     this.getOnlyElement()
+  }
+
+   deleteDept(){
+    console.log('delete')
+    let elem1 = document.getElementsByClassName("OriginalDiv")[this.countBoss]
+    elem1.remove()
+    this.countBoss--
+  }
+
+  addPos() {
+    const add = document.getElementById("add-pos")!
+    const clone = add.cloneNode(true)
+    document.getElementById("pos")?.appendChild(clone) 
+    this.countDept++
+ }
 }
