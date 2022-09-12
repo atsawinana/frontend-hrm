bd66b9469eae945201278659c44cc37af03fd92d
atsawinana <atsawin443@hotmail.com>
2022-09-12 23:06:18 +0700

    update script
    Created at : 12/09/2565
    Update at : 12/09/2565
    Author : Atsawin Anantasri
    Detail: update script for change css class

--- src/app/main/main-component/side-bar/side-bar/side-bar.component.ts
+++ src/app/main/main-component/side-bar/side-bar/side-bar.component.ts
@@ -15,32 +15,79 @@ export class SideBarComponent implements OnInit {
 
   navigateActive() {
     if (this.router.url.includes('profile')) {
-      const addClass = document.getElementById('profile');
-      addClass?.classList.add('bg-active');
+      const addClass = document.querySelectorAll('#profile');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
     } else if (this.router.url.includes('timeattendance')) {
-      const addClass = document.getElementById('timeattendance');
-      addClass?.classList.add('bg-active');
-    } else if (this.router.url.includes('employee')) {
-      const addClass = document.getElementById('employee');
-      addClass?.classList.add('bg-active');
-    } else if (this.router.url.includes('department')) {
-      const addClass = document.getElementById('department');
-      addClass?.classList.add('bg-active');
-    } else if (this.router.url.includes('leave')) {
-      const addClass = document.getElementById('leave');
-      addClass?.classList.add('bg-active');
-    } else if (this.router.url.includes('ot')) {
-      const addClass = document.getElementById('ot');
-      addClass?.classList.add('bg-active');
+      const addClass = document.querySelectorAll('#timeattendance');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
     }
-    if (this.router.url.includes('car')) {
-      const addClass = document.getElementById('car');
-      addClass?.classList.add('bg-active');
+    else if (this.router.url.includes('department')) {
+      const addClass = document.querySelectorAll('#department');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
     }
-    if (this.router.url.includes('activity')) {
-      const addClass = document.getElementById('activity');
-      addClass?.classList.add('bg-active');
+    else if (this.router.url.includes('leave')) {
+      const addClass = document.querySelectorAll('#leave');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
     }
+    else if (this.router.url.includes('ot')) {
+      const addClass = document.querySelectorAll('#ot');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
+    }
+    else if (this.router.url.includes('car')) {
+      const addClass = document.querySelectorAll('#car');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
+    }
+    else if (this.router.url.includes('activity')) {
+      const addClass = document.querySelectorAll('#activity');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
+    }
+    else if (this.router.url.includes('employee')) {
+      const addClass = document.querySelectorAll('#employee');
+      for (let i = 0; i < addClass.length; i++) {
+        addClass[i].classList.add('bg-active');
+      } 
+    }
+
+
+
+
+
+    // } else if (this.router.url.includes('timeattendance')) {
+    //   const addClass = document.getElementById('timeattendance');
+    //   addClass?.classList.add('bg-active');
+    // } else if (this.router.url.includes('employee')) {
+    //   const addClass = document.getElementById('employee');
+    //   addClass?.classList.add('bg-active');
+    // } else if (this.router.url.includes('department')) {
+    //   const addClass = document.getElementById('department');
+    //   addClass?.classList.add('bg-active');
+    // } else if (this.router.url.includes('leave')) {
+    //   const addClass = document.getElementById('leave');
+    //   addClass?.classList.add('bg-active');
+    // } else if (this.router.url.includes('ot')) {
+    //   const addClass = document.getElementById('ot');
+    //   addClass?.classList.add('bg-active');
+    // } else if (this.router.url.includes('car')) {
+    //   const addClass = document.getElementById('car');
+    //   addClass?.classList.add('bg-active');
+    // } else if (this.router.url.includes('activity')) {
+    //   const addClass = document.getElementById('activity');
+    //   addClass?.classList.add('bg-active');
+    // }
   }
 
   URLcheck(event: any) {
