6f9bf56f927252e727a8d21116ff107061ee00a4
Atsawin017 <atsawin443@hotmail.com>
2022-09-14 20:05:45 +0700

    update script
    Created at : 15/08/2565
    Update at : 19/09/2565
    Author : Atsawin Anantasri
    Detail : update script add button

--- src/app/main/sub-component/department/add-department/add-department.component.ts
+++ src/app/main/sub-component/department/add-department/add-department.component.ts
@@ -1,32 +1,76 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { AdddepartmentService } from './adddepartment.service';
 
 @Component({
   selector: 'app-add-department',
   templateUrl: './add-department.component.html',
-  styleUrls: ['./add-department.component.css']
+  styleUrls: ['./add-department.component.css'],
 })
 export class AddDepartmentComponent implements OnInit {
   name = 'Dynamic Add Fields';
-  values = [];
   namedepartment_en = new FormControl('');
   namedepartment_th = new FormControl('');
   nameleader = new FormControl('');
   naemposition = new FormControl('');
   isSuccess?: boolean;
+  OpString!: number[];
+  DeptMana: string[] = [];
 
-  constructor(private _fb: FormBuilder,private Add_dp: AdddepartmentService) {
-    this.bossForm = this._fb.group({
-      boss: this._fb.array([this.addBossField()])
-    });
-    this.posForm = this._fb.group({
-      pos: this._fb.array([this.addPosField()])
-    });
-  }
+  constructor(private Add_dp: AdddepartmentService) {}
 
   ngOnInit() {
+    this.OpString = [1];
   }
+
+  onSubmit() {
+    this.ClearAlertText_dp();
+    this.ClearAlertText_lead();
+    this.ClearAlertText_pos();
+
+    this.Add_dp.adddepartment(
+      this.namedepartment_en.value!,
+      this.namedepartment_th.value!,
+      this.naemposition.value!,
+      this.nameleader.value!
+    ).subscribe({
+      next: (res: any) => {
+        console.log('Success, input are correct');
+        this.isSuccess = true;
+      },
+      error: (err) => {
+        console.log('Failed, input is null');
+        this.isSuccess = false;
+        if (this.isSuccess == false) {
+          if (this.namedepartment_en.value == '') {
+            console.log('not input');
+            this.alertTextRedNull_namedp();
+          }
+
+          if (this.naemposition.value == '') {
+            console.log('not input');
+            this.alertTextRedNull_namepos();
+          }
+
+          if (this.nameleader.value == '') {
+            console.log('not input');
+            this.alertTextRedNull_namelead();
+          }
+        }
+      },
+    });
+  }
+
+  addInputDept() {
+    console.log(this.DeptMana[this.OpString.length - 1])
+    if (this.DeptMana[this.OpString.length - 1] == null) {
+      alert('cannot กรอกให้ครบหน่อย');
+    } else {
+      this.OpString?.push(this.OpString[this.OpString.length - 1] + 1);
+    }
+  }
+
+  deleteInputDept() {}
 
   alertTextRedNull_namedp() {
     let alert = document.getElementById('alertnull_dp');
@@ -57,92 +101,4 @@ export class AddDepartmentComponent implements OnInit {
     let alertnull = document.getElementById('alertnull_lead');
     alertnull!.style.display = 'none';
   }
-
-  onSubmit()
-  {
-    this.ClearAlertText_dp();
-    this.ClearAlertText_lead();
-    this.ClearAlertText_pos();
-
-    this.Add_dp.adddepartment(this.namedepartment_en.value!,this.namedepartment_th.value!,this.naemposition.value!,this.nameleader.value!).subscribe({
-      next: (res: any) => {
-        console.log('Success, input are correct');
-        this.isSuccess = true;
-      },
-      error: (err) => {
-        console.log('Failed, input is null');
-        this.isSuccess = false;
-          if (this.isSuccess == false) {
-            if (this.namedepartment_en.value == '') {
-              console.log('not input');
-              this.alertTextRedNull_namedp();
-            }
-
-            if (this.naemposition.value == '') {
-              console.log('not input');
-              this.alertTextRedNull_namepos();
-            }
-
-            if (this.nameleader.value == '') {
-              console.log('not input');
-              this.alertTextRedNull_namelead();
-            }
-          }
-      }
-  })
-  }
-
-
-  public bossForm: FormGroup;
-  public posForm: FormGroup;
-  
-  
-
-  //Append Boss Fields Set
-  private addBossField(): FormGroup {
-    return this._fb.group({
-      Name: []
-    });
-  }
-  //Add Boss Fields
-  addBoss(): void {
-    this.bossArray.push(this.addBossField());
-    console.log("bossArray: ",this.bossArray.length)
-  }
-
-  //Remove Boss Fields
-  removeBoss(index: number): void {
-    this.bossArray.removeAt(index);
-    console.log("bossArray: ",this.bossArray.length)
-  }
-  //Fields Boss Array
-  get bossArray(): FormArray {
-    return <FormArray>this.bossForm.get('boss');
-  }
-
-  //Append Position Fields Set
-  private addPosField(): FormGroup {
-    return this._fb.group({
-      Position: []
-    });
-  }
-  //Add Position Fields
-  addPos(): void {
-    const check = document.getElementById('')
-    this.posArray.push(this.addPosField());
-    console.log("posArray: ",this.posArray.length)
-  }
-
-  //Remove Position Fields
-  removePos(index: number): void {
-    this.posArray.removeAt(index);
-    console.log("posArray: ",this.posArray.length)
-  }
-  //Fields Position Array
-  get posArray(): FormArray {
-    return <FormArray>this.posForm.get('pos');
-  }
-
 }
-
-
