7e3a1b2f9d51bb9cc55a245a87d32f2576274113
atsawinana <atsawin443@hotmail.com>
2022-09-19 15:44:13 +0700

    update datalist
    Created at : 15/08/2565
    Update at : 19/09/2565
    Author : Atsawin Anantasri
    Detail : update datalist for input from api service

--- src/app/main/sub-component/department/add-department/add-department.component.ts
+++ src/app/main/sub-component/department/add-department/add-department.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { DepartmentService } from '../department.service';
 import { AdddepartmentService } from './adddepartment.service';
 
 @Component({
@@ -18,30 +19,54 @@ export class AddDepartmentComponent implements OnInit {
   countDeptPosit!: number[];
   DeptMana: string[] = [];
   DeptPosit: string[] = [];
+  DeptUsername:string[] = [];
+  DeptUserID: any[] = [];
 
-  constructor(private Add_dp: AdddepartmentService) {}
+  constructor(
+    private Add_dp: AdddepartmentService,
+    private Maindept: DepartmentService
+  ) {}
 
   ngOnInit() {
+    this.Maindept.getAllUser().subscribe({
+      next: (res: any) => {
+        this.DeptUserID = res.data.users;
+      },
+      error: (err) => {
+        console.log('Failed, input is null');
+      },
+    });
+
     this.countDeptMana = [1];
     this.countDeptPosit = [1];
   }
 
-  onSubmit() {
-    // this.ClearAlertText_dp();
-    // this.ClearAlertText_lead();
-    // this.ClearAlertText_pos();
-    console.log('before')
-    console.log(this.DeptPosit)
-    console.log(this.DeptMana)
-    console.log(this.namedepartment_en.value)
-    console.log(this.namedepartment_th.value)
-    console.log('after')
+  MapUsernameWithID() {
+    console.log('check len', this.DeptUserID.length);
+    for (let i = 0; i < this.DeptMana.length; i++) {
+      for (let j = 0; j < this.DeptUserID.length; j++) {
+        
+        if(this.DeptMana[i] === this.DeptUserID[j].ud_fullname_th)
+        {
+          this.DeptUsername.push(String(this.DeptUserID[j].ud_username))
+          console.log(this.DeptUsername[i])
+        }
+        // console.log('mana i',this.DeptMana[i]);
+        // console.log('uid j',this.DeptUserID[j].ud_fullname_th,'id',this.DeptUserID[j].ud_id);
+      }
+      // if (this.DeptMana[i] == this.DeptUserID[i].ud_fullname_th) {
+      //   console.log(this.DeptUserID[i].ud_id);
+      // }
+    }
+  }
 
+  onSubmit() {
+    this.MapUsernameWithID();
     this.Add_dp.adddepartment(
       this.namedepartment_en.value!,
       this.namedepartment_th.value!,
       this.DeptPosit!,
-      this.DeptMana!
+      this.DeptUsername!
     ).subscribe({
       next: (res: any) => {
         console.log('Success, input are correct');
@@ -50,10 +75,6 @@ export class AddDepartmentComponent implements OnInit {
       error: (err) => {
         console.log('Failed, input is null');
         this.isSuccess = false;
-        // if (this.isSuccess == false) {
-        //   if (this.namedepartment_en.value == '') {
-        //     console.log('not input');
-        //     this.alertTextRedNull_namedp();
         //   }
 
         //   if (this.naemposition.value == '') {
@@ -71,31 +92,35 @@ export class AddDepartmentComponent implements OnInit {
   }
 
   addInputDept() {
-    console.log(this.DeptMana[this.countDeptMana.length - 1])
+    // console.log(this.DeptMana);
     if (this.DeptMana[this.countDeptMana.length - 1] == null) {
       alert('cannot กรอกให้ครบหน่อย');
     } else {
-      this.countDeptMana?.push(this.countDeptMana[this.countDeptMana.length - 1] + 1);
+      this.countDeptMana?.push(
+        this.countDeptMana[this.countDeptMana.length - 1] + 1
+      );
     }
   }
 
   addInputDeptPosit() {
-    console.log(this.DeptPosit[this.countDeptPosit.length - 1])
+    // console.log(this.DeptPosit[this.countDeptPosit.length - 1]);
     if (this.DeptPosit[this.countDeptPosit.length - 1] == null) {
       alert('cannot กรอกให้ครบหน่อย');
     } else {
-      this.countDeptPosit?.push(this.countDeptPosit[this.countDeptPosit.length - 1] + 1);
+      this.countDeptPosit?.push(
+        this.countDeptPosit[this.countDeptPosit.length - 1] + 1
+      );
     }
   }
 
-  deleteDept(index:number){
-    this.countDeptMana.splice(index,1)
-    this.DeptMana.splice(index,1)
+  deleteDept(index: number) {
+    this.countDeptMana.splice(index, 1);
+    this.DeptMana.splice(index, 1);
   }
 
-  deleteInputDept(index:number) {
-    this.countDeptPosit.splice(index,1)
-    this.DeptPosit.splice(index,1)
+  deleteInputDept(index: number) {
+    this.countDeptPosit.splice(index, 1);
+    this.DeptPosit.splice(index, 1);
   }
 
   alertTextRedNull_namedp() {
--- src/app/main/sub-component/department/add-department/adddepartment.service.ts
+++ src/app/main/sub-component/department/add-department/adddepartment.service.ts
@@ -2,23 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdddepartmentService {
+  constructor(private httpClient: HttpClient) {}
 
-constructor(private httpClient: HttpClient) { }
+  adddepartment(
+    namedepartment_en: string,
+    namedepartment_th: string,
+    naemposition: string[],
+    nameleader: string[]
+  ) {
+    console.log(naemposition);
+    console.log(nameleader);
+    return this.httpClient.post(
+      'http://127.0.0.1:8000/api/department/addDepartment',
+      {
+        dept_name_en: namedepartment_en,
+        dept_name_th: namedepartment_th,
+        dp_name_en: naemposition,
+        dp_name_th: 'เทสภาษาไทย',
+        dept_update_by: 'FixID',
+        dmm_username: nameleader,
+      }
+    );
+  }
 
-adddepartment(namedepartment_en: string, namedepartment_th: string, naemposition: string[],nameleader: string[]) {
-  console.log(naemposition)
-  console.log(nameleader)
-  return this.httpClient.post('http://127.0.0.1:8000/api/department/addDepartment', {
-    dept_name_en: namedepartment_en,
-    dept_name_th: namedepartment_th,
-    dp_name_en : naemposition,
-    dp_name_th : "เทสภาษาไทย",
-    dept_update_by : "FixID",
-    dmm_username: nameleader,
-  })
-}
-
+  
 }
