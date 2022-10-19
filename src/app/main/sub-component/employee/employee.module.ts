b1cd3db804102773d45115422b854ee7ca9dcdfa
atsawinana <atsawin443@hotmail.com>
2022-10-19 21:12:34 +0700

    update structure employee
    Created at : 19/10/2565
    Update at : 19/10/2565
    Author : Atsawin Anantasri
    Detail : update structure employee

--- src/app/main/sub-component/employee/employee.module.ts
+++ src/app/main/sub-component/employee/employee.module.ts
@@ -5,17 +5,27 @@ import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { RouterModule } from '@angular/router';
 import { DataPersonComponent } from './data-person/data-person.component';
 import { EmployeeRoutes } from './employee.routing';
+import { DetailDataPersonComponent } from './data-person/person-employee/detail-data-person/detail-data-person.component';
+import { LeaveHistoryPersonComponent } from './data-person/person-employee/leave-history-person/leave-history-person.component';
+import { OtHistoryPersonComponent } from './data-person/person-employee/ot-history-person/ot-history-person.component';
+import { WorkHistoryPersonComponent } from './data-person/person-employee/work-history-person/work-history-person.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    EmployeeRoutes
+    EmployeeRoutes,
+    FormsModule
   ],
   declarations: [
     ListEmployeeComponent,
     AddEmployeeComponent,
-    DataPersonComponent
+    DataPersonComponent,
+    DetailDataPersonComponent,
+    LeaveHistoryPersonComponent,
+    OtHistoryPersonComponent,
+    WorkHistoryPersonComponent
   ]
 })
 export class EmployeeModule { }
