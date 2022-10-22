2ea1a4a5264fe3a528b13b259c624c31ba45d7fa
63160253 <63160253@go.buu.ac.th>
2022-10-22 23:36:03 +0700

    request Leave online
    Created at : 20/10/2565
    Update at : 22/10/2565
    Author : Pichayada rongchapho
    Detail :  request Leave online

--- src/app/main/sub-component/leave/leave-request/leave-request.component.ts
+++ src/app/main/sub-component/leave/leave-request/leave-request.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-leave-request',
   templateUrl: './leave-request.component.html',
-  styleUrls: ['./leave-request.component.css']
+  styleUrls: ['./leave-request.component.css'],
 })
 export class LeaveRequestComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  checkCancel() {
+    Swal.fire({
+      title:
+        '<strong style = "font-family:Kanit"> คุณต้องการแก้ไขแผนกใช่หรือไม่ </strong>',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
+      confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
+      confirmButtonColor: '#005FBC',
+      reverseButtons: true,
+    });
   }
-
+  checkNull() {
+    Swal.fire({
+      title:
+        '<strong style = "font-family:Kanit"> คุณต้องการแก้ไขแผนกใช่หรือไม่ </strong>',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
+      confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
+      confirmButtonColor: '#005FBC',
+      reverseButtons: true,
+    });
+  }
 }
