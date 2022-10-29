67eea8b7ea2027c9c502917ef331061b2f3f70e1
63160253 <63160253@go.buu.ac.th>
2022-10-29 17:56:47 +0700

    edit detail data person and add edit detail person
    Created at : 28/10/2565
    Update at : 29/10/2565
    Author : pichayada rongchapho
    Detail : edit detail data person and add edit detail person

--- src/app/main/sub-component/employee/data-person/person-employee/edit-detail-person/edit-detail-person.component.ts
+++ src/app/main/sub-component/employee/data-person/person-employee/edit-detail-person/edit-detail-person.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { defineLocale, thBeLocale } from 'ngx-bootstrap/chronos';
+import Swal from 'sweetalert2';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-edit-detail-person',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditDetailPersonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private localeService: BsLocaleService) { }
+  locale = 'th';
+    today!: Date;
 
   ngOnInit() {
+    this.today = new Date();
+        defineLocale('th', thBeLocale);
+        this.localeService.use(this.locale);
   }
 
 }
