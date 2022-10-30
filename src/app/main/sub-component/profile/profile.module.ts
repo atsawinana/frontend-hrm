1b4a1378390f7c651c18e2bd5b8cedfb5db46945
tanathonch <63160090@go.buu.ac.th>
2022-10-31 00:25:35 +0700

    Update change password
    Created at : 19/10/2565
    Update at : 31/10/2565
    Author : Tanathon Chalowthammaporn
    Detail :  Update change password api

--- src/app/main/sub-component/profile/profile.module.ts
+++ src/app/main/sub-component/profile/profile.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ProfileRoutes } from './profile.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     ProfileRoutes,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  declarations: [ProfileComponent,ResetPasswordComponent]
+  declarations: [ProfileComponent, ResetPasswordComponent],
 })
 export class ProfileModule { }
