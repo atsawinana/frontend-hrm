845d05e7b31f4ce09acba1703b600c036470738e
atsawinana <atsawin443@hotmail.com>
2022-10-30 21:58:42 +0700

    Update profile
    Created at : 19/10/2565
    Update at : 30/10/2565
    Author : Atsawin Anantasri
    Detail : Update profile

--- src/app/main/sub-component/profile/profile.component.ts
+++ src/app/main/sub-component/profile/profile.component.ts
@@ -9,6 +9,7 @@ import {
 } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { ProfileService } from './profile.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile',
@@ -24,7 +25,7 @@ export class ProfileComponent implements OnInit {
   testleaveday: any;
   ObjTestCircle1 = { l1: 4, l2: 12, l3: 7 };
   ObjTestCircle2 = { l1: 13, l2: 2, l3: 3, l4: 5 };
-
+  baseURL = environment.apiURL;
   //  กราฟผู้ชาย
   public ChartData: ChartData<'bar'> = {
     datasets: [
