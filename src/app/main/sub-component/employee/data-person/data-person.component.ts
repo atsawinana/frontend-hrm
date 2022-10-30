c8706a32db2cb49954cdd8a84186ceb0e0383448
atsawinana <atsawin443@hotmail.com>
2022-10-30 23:31:44 +0700

    Update employee profile
    Created at : 30/10/2565
    Update at : 30/10/2565
    Author : Atsawin Anantasri
    Detail : Update employee profile

--- src/app/main/sub-component/employee/data-person/data-person.component.ts
+++ src/app/main/sub-component/employee/data-person/data-person.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataPersonService } from './data-person.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-data-person',
   templateUrl: './data-person.component.html',
-  styleUrls: ['./data-person.component.css']
+  styleUrls: ['./data-person.component.css'],
 })
 export class DataPersonComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private dataService: DataPersonService,
+    private router: ActivatedRoute
+  ) {}
+  Empid: any;
+  baseURL = environment.apiURL;
+  ObjDataemp: any;
+  APISuccess:boolean = false
   ngOnInit() {
+    this.Empid = this.router.snapshot.params['id'];
+    localStorage.setItem('empPerson',this.Empid)
+    this.dataService.getUserProfile(this.Empid).subscribe({
+      next: (res: any) => {
+        this.ObjDataemp = res.data
+        console.log(this.ObjDataemp)
+        if(this.ObjDataemp.ud_picture == null) {
+            this.ObjDataemp.ud_picture = "/files/image/default.jpg"
+        } 
+        this.APISuccess = true
+      },
+      error: (error: any) => {},
+    });
   }
-
 }
--- src/app/main/sub-component/employee/data-person/person-employee/detail-data-person/detail-data-person.component.ts
+++ src/app/main/sub-component/employee/data-person/person-employee/detail-data-person/detail-data-person.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { DataPersonService } from '../../data-person.service';
 
 @Component({
   selector: 'app-detail-data-person',
   templateUrl: './detail-data-person.component.html',
-  styleUrls: ['./detail-data-person.component.css']
+  styleUrls: ['./detail-data-person.component.css'],
 })
 export class DetailDataPersonComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private dataService: DataPersonService,
+    private router: ActivatedRoute
+  ) {}
+  Empid: any;
+  baseURL = environment.apiURL;
+  ObjDataemp: any;
+  APISuccess:boolean = false
   ngOnInit() {
-  }
+    this.Empid = localStorage.getItem('empPerson')
 
+    this.dataService.getUserProfile(this.Empid).subscribe({
+        next: (res: any) => {
+          this.ObjDataemp = res.data
+          console.log(this.ObjDataemp)
+          if(this.ObjDataemp.ud_picture == null) {
+              this.ObjDataemp.ud_picture = "/files/image/default.jpg"
+          } 
+          this.APISuccess = true
+        },
+        error: (error: any) => {},
+      });
+  }
 }
