792ad6de20346b336c2db6e0edeb74ca8300c73e
atsawinana <atsawin443@hotmail.com>
2022-11-01 00:20:33 +0700

    Update employee
    Created at : 30/10/2565
    Update at : 1/11/2565
    Author : Atsawin Anantasri
    Detail : Update employee

--- src/app/main/sub-component/profile/profile.module.ts
+++ src/app/main/sub-component/profile/profile.module.ts
@@ -6,9 +6,10 @@ import { ProfileRoutes } from './profile.routing';
 import { NgChartsModule } from 'ng2-charts';
 import { ChartOtComponent } from './chart-ot/chart-ot.component';
 import { ChartLeaveComponent } from './chart-leave/chart-leave.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, ProfileRoutes, NgChartsModule],
+  imports: [CommonModule, ProfileRoutes, NgChartsModule, ReactiveFormsModule],
   declarations: [
     ProfileComponent,
     ResetPasswordComponent,
