ca93ef74fc41f84fb706d59356f23527f60e4d5c
atsawinana <atsawin443@hotmail.com>
2022-10-31 23:24:08 +0700

    Update employee profile
    Created at : 30/10/2565
    Update at : 31/10/2565
    Author : Atsawin Anantasri
    Detail : Update employee profile

---
+++ src/app/main/sub-component/profile/chart-leave/chart-leave.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-leave',
+  templateUrl: './chart-leave.component.html',
+  styleUrls: ['./chart-leave.component.css']
+})
+export class ChartLeaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/main/sub-component/profile/chart-ot/chart-ot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-ot',
+  templateUrl: './chart-ot.component.html',
+  styleUrls: ['./chart-ot.component.css']
+})
+export class ChartOtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
