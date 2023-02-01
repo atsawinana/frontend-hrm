59d87252e3d50df93e559eb3234c12b6d47fafd2
atsawinana <atsawin443@hotmail.com>
2023-02-01 13:34:02 +0700

    update route
    Created at : 22/11/2565
    Update at : 1/2/2566
    Author : Atsawin Anantasri
    Detail : update route

--- src/app/main/main.routing.ts
+++ src/app/main/main.routing.ts
@@ -1,51 +1,23 @@
-import { Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LeaveComponent } from './sub-component/leave/leave.component';
-import { MainComponent } from './main-component/main.component';
 import { OtComponent } from './sub-component/ot/ot.component';
-import { ProfileComponent } from './sub-component/profile/profile.component';
-import { TimeAttendanceComponent } from './sub-component/timeAttendance/timeAttendance.component';
 import { TravelExpensesComponent } from './sub-component/travelExpenses/travelExpenses.component';
 import { ActivityComponent } from './sub-component/activity/activity.component';
 import { RoleGuard } from '../auth/role.guard';
+import { AuthGuardGuard } from '../auth/auth-guard.guard';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: ProfileComponent,
-  // },
-  // {
-  //   path: 'leave',
-  //   component: LeaveComponent,
-  // },
-  // {
-  //   path: 'department',
-  //   loadChildren: () => import('./sub-component/department/department.module').then((m) => m.DepartmentModule),
-  // },
-  // {
-  //   path: 'ot',
-  //   component: OtComponent
-  // },
-  // {
-  //   path: 'profile',
-  //   component: ProfileComponent,
-  // },
-  // {
-  //   path: 'timeattendance',
-  //   component: TimeAttendanceComponent,
-  // },
-  // {
-  //   path: 'employee',
-  //   loadChildren: () => import('./sub-component/employee/employee.module').then((m) => m.EmployeeModule)
-  // },
-  // {
-  //   path: 'car',
-  //   component: TravelExpensesComponent,
-  // },
-  // {
-  //   path: 'activity',
-  //   component: ActivityComponent,
-  // },
+    { path: '', loadChildren: () => import('./sub-component/profile/profile.module').then((m) => m.ProfileModule), canActivate: [AuthGuardGuard] },
+    { path: 'leave', loadChildren: () => import('./sub-component/leave/leave.module').then((m) => m.LeaveModule), canActivate: [AuthGuardGuard] },
+    {
+        path: 'department', loadChildren: () => import('./sub-component/department/department.module').then((m) => m.DepartmentModule), canActivate: [RoleGuard, AuthGuardGuard]
+    },
+    // { path: 'ot', component: OtComponent, canActivate: [AuthGuardGuard] },
+    { path: 'profile', loadChildren: () => import('./sub-component/profile/profile.module').then((m) => m.ProfileModule), canActivate: [AuthGuardGuard] },
+    { path: 'timeattendance', loadChildren: () => import("./sub-component/timeAttendance/timeAtt.module").then((m) => m.TimeAttModule), canActivate: [AuthGuardGuard] },
+    { path: 'employee', loadChildren: () => import('./sub-component/employee/employee.module').then((m) => m.EmployeeModule), canActivate: [RoleGuard, AuthGuardGuard] },
+    // { path: 'car', component: TravelExpensesComponent, canActivate: [AuthGuardGuard] },
+    // { path: 'activity', component: ActivityComponent, canActivate: [AuthGuardGuard] },
+    { path: 'notification', loadChildren: () => import('./sub-component/notification/noti.module').then((m) => m.NotiModule) },
 
 ];
 
