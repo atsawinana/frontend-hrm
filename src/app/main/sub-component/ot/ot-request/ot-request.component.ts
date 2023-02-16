73138bfa0020dac4f06566ca09d5747113837600
tanathonch <63160090@go.buu.ac.th>
2023-02-16 19:56:31 +0700

    OT request
    Created at : 22/11/2566
    Update at : 16/2/2566
    Author : Tanathon Chalowthammaporn
    Detail : ot request html

--- src/app/main/sub-component/ot/ot-request/ot-request.component.ts
+++ src/app/main/sub-component/ot/ot-request/ot-request.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { defineLocale, thBeLocale } from 'ngx-bootstrap/chronos';
+import { ActivatedRoute, Route, Router, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { DatePipe } from '@angular/common';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-ot-request',
   templateUrl: './ot-request.component.html',
@@ -7,9 +14,239 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtRequestComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private localeService: BsLocaleService,
+    private route: Router,
+    private datepipe: DatePipe,
+    private _location: Location
+  ) { }
+
+  locale = 'th';
+  today!: Date;
+  summited: boolean = false;
+  requestStatus = 1
+
+  time = {
+    hours: 0,
+    alerthours: false,
+    mins: 0,
+    alertmins: false,
+  }
+  Request = new FormGroup({
+    Type: new FormControl(null, [Validators.required]),
+    Date: new FormControl('', [Validators.required]),
+    startTime: new FormControl('', [Validators.required]),
+    EndTime: new FormControl('', [Validators.required]),
+    detail: new FormControl('', [
+      Validators.required,
+      this.noWhitespaceValidator,
+    ]),
+  });
 
   ngOnInit() {
+    this.today = new Date();
+    defineLocale('th', thBeLocale);
+    this.localeService.use(this.locale);
   }
 
+  noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
+  }
+
+  backClicked() {
+    this._location.back();
+  }
+
+  checkCancel() {
+    Swal.fire({
+      title:
+        '<strong style = "font-family:Kanit"> คุณต้องการยกเลิกแบบฟอร์มการทำโอที ใช่หรือไม่ </strong>',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
+      confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
+      confirmButtonColor: '#005FBC',
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.route.navigate(['../main/ot']);
+      }
+    });
+  }
+
+  // onValueChange(event: any) {
+  //   console.log(this.Request.controls.Date.value!)
+  //   let a = new Date(this.Request.controls.Date.value!)
+  //   console.log(a.toLocaleDateString('th-TH'))
+  //   console.log(a.toLocaleDateString('en-US'))
+
+  //   let date = this.datepipe.transform(this.Request.controls.Date.value, 'yyyy-MM-dd');
+  //   this.serviceTimeatd.checkRequestAttendance(date).subscribe({
+  //     next: (res: any) => {
+  //       this.requestStatus = res.data.status
+  //       console.log(res.data.status)
+  //       if (this.requestStatus == 0) {
+  //         Swal.fire({
+  //           title: '<strong style = "font-family:Kanit"> วันนี้คุณส่งคำขอเข้างานไปแล้ว </strong>',
+  //           icon: 'warning',
+  //           showConfirmButton: false,
+  //           timer: 1500
+  //         })
+  //         return
+  //       }
+  //     },
+  //     error: (err: any) => { }
+  //   })
+  // }
+
+  upHours() {
+    this.time.hours++
+
+    if (this.time.hours > 23) {
+      this.time.hours = 0
+    }
+  }
+
+  downHours() {
+    this.time.hours--
+
+    if (this.time.hours < 0) {
+      this.time.hours = 23
+    }
+  }
+
+  upMins() {
+    this.time.mins++
+
+    if (this.time.mins > 60) {
+      this.time.mins = 0
+    }
+  }
+
+  downMins() {
+    this.time.mins--
+
+    if (this.time.mins < 0) {
+      this.time.mins = 60
+    }
+  }
+
+  showStartTimeBox() {
+    var x = document.getElementById('StartTimeBox');
+    if (x?.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+
+      if ((this.time.hours > 24 || this.time.hours < 0 || this.time.mins > 60 || this.time.mins < 0) || (this.time.hours == null || this.time.mins == null)) {
+
+        this.time.alerthours = true
+        this.time.alertmins = true
+
+        return
+      } else {
+        this.time.alerthours = false
+        this.time.alertmins = false
+      }
+
+      if (this.time.hours == 0 && this.time.mins == 0) {
+        x!.style.display = 'none';
+        return
+      }
+      this.Request.controls.startTime.setValue(this.time.hours.toString() + " โมง " + this.time.mins.toString() + " นาที")
+      x!.style.display = 'none';
+    }
+  }
+
+  showEndTimeBox() {
+    var x = document.getElementById('EndTimeBox');
+    if (x?.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+
+      if ((this.time.hours > 24 || this.time.hours < 0 || this.time.mins > 60 || this.time.mins < 0) || (this.time.hours == null || this.time.mins == null)) {
+
+        this.time.alerthours = true
+        this.time.alertmins = true
+
+        return
+      } else {
+        this.time.alerthours = false
+        this.time.alertmins = false
+      }
+
+      if (this.time.hours == 0 && this.time.mins == 0) {
+        x!.style.display = 'none';
+        return
+      }
+      this.Request.controls.EndTime.setValue(this.time.hours.toString() + " โมง " + this.time.mins.toString() + " นาที")
+      x!.style.display = 'none';
+    }
+  }
+
+  // submitButton() {
+
+  //   this.summited = true
+
+  //   if (this.requestStatus == 0) {
+  //     Swal.fire({
+  //       title: '<strong style = "font-family:Kanit"> วันนี้คุณส่งคำขอเข้างานไปแล้ว </strong>',
+  //       icon: 'warning',
+  //       showConfirmButton: false,
+  //       timer: 1500
+  //     })
+  //     return
+  //   }
+
+  //   if (this.Request.invalid)
+  //     return
+
+  //   Swal.fire({
+  //     title: '<strong style = "font-family:Kanit"> คุณต้องการส่งแบบฟอร์มการลา ใช่หรือไม่ </strong>',
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     cancelButtonColor: '#d33',
+  //     cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
+  //     confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
+  //     confirmButtonColor: '#005FBC',
+  //     reverseButtons: true,
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
+
+  //       let min, hour
+
+  //       if (String(this.time.mins).length == 1) {
+  //         min = "0" + this.time.mins
+  //       } else {
+  //         min = String(this.time.mins)
+  //       }
+
+  //       if (String(this.time.hours).length == 1) {
+  //         hour = "0" + this.time.hours
+  //       } else {
+  //         hour = String(this.time.hours)
+  //       }
+
+  //       let time = hour + ":" + min;
+  //       let date = this.datepipe.transform(this.Request.controls.Date.value, 'yyyy-MM-dd');
+
+
+  //       this.serviceTimeatd.requestAttendance(
+  //         this.Request.controls.Type.value,
+  //         date,
+  //         time,
+  //         this.Request.controls.detail.value,
+  //       ).subscribe({
+  //         next: (res: any) => {
+  //         },
+  //         error: (err: any) => { }
+  //       })
+  //       this.backClicked()
+  //     }
+  //   });
+
+
+  // }
 }
