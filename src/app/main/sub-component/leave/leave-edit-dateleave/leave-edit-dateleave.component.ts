ae1a357baca6388bf0d283c51d2e5f034fcb6b70
atsawinana <atsawin443@hotmail.com>
2023-02-23 23:39:43 +0700

    update
    Created at : 22/11/2566
    Update at : 23/2/2566
    Author : Atsawin Anantasri
    Detail : update

--- src/app/main/sub-component/leave/leave-edit-dateleave/leave-edit-dateleave.component.ts
+++ src/app/main/sub-component/leave/leave-edit-dateleave/leave-edit-dateleave.component.ts
@@ -4,131 +4,168 @@ import { ActivatedRoute, Route, Router, RouterLink } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LeaveEditDateleaveService } from './leave-edit-dateleave.service';
 import { Location } from '@angular/common';
+import { LeaveService } from '../leave.service';
 
 @Component({
-  selector: 'app-leave-edit-dateleave',
-  templateUrl: './leave-edit-dateleave.component.html',
-  styleUrls: ['./leave-edit-dateleave.component.css']
+    selector: 'app-leave-edit-dateleave',
+    templateUrl: './leave-edit-dateleave.component.html',
+    styleUrls: ['./leave-edit-dateleave.component.css']
 })
 
 export class LeaveEditDateleaveComponent implements OnInit {
 
-  constructor(
-    private route: Router,
-    private _location: Location,
-    private editService: LeaveEditDateleaveService,
-  ) { }
-  leaveDays = new FormGroup({
-    leave: new FormControl('', [Validators.required]),
-    sick : new FormControl('', [Validators.required]),
-    takeAnnual1 : new FormControl('', [Validators.required]),
-    takeAnnual2 : new FormControl('', [Validators.required]),
-    takeAnnual3 : new FormControl('', [Validators.required]),
-    takeAnnual4 : new FormControl('', [Validators.required]),
-    ordination : new FormControl('', [Validators.required]),
-    maternity : new FormControl('', [Validators.required]),
-    militaryService : new FormControl('', [Validators.required]),
-  });
-  objData: any;
-  APISuccess: boolean = false
-  summited: boolean = false;
-  today!: Date;
-  
-  
-  ngOnInit() {
-
-    this.editService.showPresetVacation().subscribe({
-      next: (res: any) => {
-          this.objData = res.data
-          this.leaveDays.controls.leave.setValue(this.objData.amout_vacation.av_leave)
-          this.leaveDays.controls.sick.setValue(this.objData.amout_vacation.av_sick)
-          this.leaveDays.controls.takeAnnual1.setValue(this.objData.amout_vacation.av_take_annual1)
-          this.leaveDays.controls.takeAnnual2.setValue(this.objData.amout_vacation.av_take_annual2)
-          this.leaveDays.controls.takeAnnual3.setValue(this.objData.amout_vacation.av_take_annual3)
-          this.leaveDays.controls.takeAnnual4.setValue(this.objData.amout_vacation.av_take_annual4)
-          this.leaveDays.controls.ordination.setValue(this.objData.amout_vacation.av_ordination)
-          this.leaveDays.controls.maternity.setValue(this.objData.amout_vacation.av_maternity)
-          this.leaveDays.controls.militaryService.setValue(this.objData.amout_vacation.av_military_service)
-          // console.log(res.data)
-          // console.log(this.leaveDays)
-          this.APISuccess = true
-      },
-      error: (err: any) => {
-      }
-  });
-
-  }
-  
-  
-
-  checkCancel() {
-    Swal.fire({
-        title:
-            '<strong style = "font-family:Kanit"> คุณต้องการยกเลิกการแก้ไขวันลา ใช่หรือไม่ </strong>',
-        icon: 'warning',
-        showCancelButton: true,
-        cancelButtonColor: '#d33',
-        cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
-        confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
-        confirmButtonColor: '#005FBC',
-        reverseButtons: true,
-    }).then((result) => {
-        if (result.isConfirmed) {
-            this.route.navigate(['../main/leave']);
-        }
+    constructor(
+        private route: Router,
+        private _location: Location,
+        private editService: LeaveEditDateleaveService,
+        private leaveService: LeaveService
+    ) { }
+    leaveDays = new FormGroup({
+        leave: new FormControl('', [Validators.required]),
+        sick: new FormControl('', [Validators.required]),
+        ordination: new FormControl('', [Validators.required]),
+        maternity: new FormControl('', [Validators.required]),
+        militaryService: new FormControl('', [Validators.required]),
+        v_take_annual0_12: new FormControl('', [Validators.required]),
+        v_take_annual13_15: new FormControl('', [Validators.required]),
+        v_take_annual16_18: new FormControl('', [Validators.required]),
+        v_take_annual19_21: new FormControl('', [Validators.required]),
+        v_take_annual22_24: new FormControl('', [Validators.required]),
+        v_take_annual25_27: new FormControl('', [Validators.required]),
+        v_take_annual28_30: new FormControl('', [Validators.required]),
+        v_take_annual31_33: new FormControl('', [Validators.required]),
+        v_take_annual34_36: new FormControl('', [Validators.required]),
+        v_take_annual37_39: new FormControl('', [Validators.required]),
+        v_take_annual40_42: new FormControl('', [Validators.required]),
+        v_take_annual43_45: new FormControl('', [Validators.required]),
+        v_take_annual46: new FormControl('', [Validators.required]),
+        v_without_pay: new FormControl('', [Validators.required]),
+        v_resign: new FormControl('', [Validators.required]),
     });
-  }
-  checkSummit(){
-    this.summited = true;
-    if (this.leaveDays.invalid)
-    return;
+    objData: any;
+    APISuccess: boolean = false
+    summited: boolean = false;
+    today!: Date;
 
-    Swal.fire({
-      title: '<strong style = "font-family:Kanit"> คุณต้องการบันทึกการเเก้ไขวันลา ใช่หรือไม่ </strong>',
-      icon: 'warning',
-      showCancelButton: true,
-      cancelButtonColor: '#d33',
-      cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
-      confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
-      confirmButtonColor: '#005FBC',
-      reverseButtons: true,
-  }).then((e) => {
-      if (e.isConfirmed) {
-          this.editService.editVacationDays(
-            this.leaveDays.controls.leave.value, 
-            this.leaveDays.controls.sick.value, 
-            this.leaveDays.controls.takeAnnual1.value, 
-            this.leaveDays.controls.takeAnnual2.value, 
-            this.leaveDays.controls.takeAnnual3.value, 
-            this.leaveDays.controls.takeAnnual4.value,
-            this.leaveDays.controls.ordination.value,
-            this.leaveDays.controls.maternity.value,
-            this.leaveDays.controls.militaryService.value
-          ).subscribe({
-              next: (res: any) => {
-                this.route.navigate(['../main/leave']);
-              },
-              error: (res: any) => {
-              }
-          });
-      }
-  })
-    
-  }
+    ngOnInit() {
 
+        this.leaveService.getleaveOnlineEdit().subscribe({
+            next: (res: any) => {
+                this.objData = res.data
+                this.leaveDays.controls.leave.setValue(this.objData.vacation.v_leave)
+                this.leaveDays.controls.sick.setValue(this.objData.vacation.v_sick)
+                this.leaveDays.controls.ordination.setValue(this.objData.vacation.v_ordination)
+                this.leaveDays.controls.maternity.setValue(this.objData.vacation.v_maternity)
+                this.leaveDays.controls.militaryService.setValue(this.objData.vacation.v_military_service)
+                this.leaveDays.controls.v_take_annual0_12.setValue(this.objData.vacation.v_take_annual0_12)
+                this.leaveDays.controls.v_take_annual13_15.setValue(this.objData.vacation.v_take_annual13_15)
+                this.leaveDays.controls.v_take_annual16_18.setValue(this.objData.vacation.v_take_annual16_18)
+                this.leaveDays.controls.v_take_annual19_21.setValue(this.objData.vacation.v_take_annual19_21)
+                this.leaveDays.controls.v_take_annual22_24.setValue(this.objData.vacation.v_take_annual22_24)
+                this.leaveDays.controls.v_take_annual25_27.setValue(this.objData.vacation.v_take_annual25_27)
+                this.leaveDays.controls.v_take_annual28_30.setValue(this.objData.vacation.v_take_annual28_30)
+                this.leaveDays.controls.v_take_annual31_33.setValue(this.objData.vacation.v_take_annual31_33)
+                this.leaveDays.controls.v_take_annual34_36.setValue(this.objData.vacation.v_take_annual34_36)
+                this.leaveDays.controls.v_take_annual37_39.setValue(this.objData.vacation.v_take_annual37_39)
+                this.leaveDays.controls.v_take_annual40_42.setValue(this.objData.vacation.v_take_annual40_42)
+                this.leaveDays.controls.v_take_annual43_45.setValue(this.objData.vacation.v_take_annual43_45)
+                this.leaveDays.controls.v_take_annual46.setValue(this.objData.vacation.v_take_annual46)
+                this.leaveDays.controls.v_without_pay.setValue(this.objData.vacation.v_without_pay),
+                this.leaveDays.controls.v_resign.setValue(this.objData.vacation.v_resign)
 
-  inputNumberOnly(event: any): boolean {
-    const charCode = event.which ? event.which : event.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-      return false;
+                
+                // console.log(res.data)
+                // console.log(this.leaveDays)
+                this.APISuccess = true
+            },
+            error: (err: any) => {
+            }
+        });
+
     }
-    return true;
-  }
+
+
+
+    checkCancel() {
+        Swal.fire({
+            title:
+                '<strong style = "font-family:Kanit"> คุณต้องการยกเลิกการแก้ไขวันลา ใช่หรือไม่ </strong>',
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
+            confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
+            confirmButtonColor: '#005FBC',
+            reverseButtons: true,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                this.route.navigate(['../main/leave']);
+            }
+        });
+    }
+    checkSummit() {
+        this.summited = true;
+        if (this.leaveDays.invalid)
+            return;
+
+        Swal.fire({
+            title: '<strong style = "font-family:Kanit"> คุณต้องการบันทึกการเเก้ไขวันลา ใช่หรือไม่ </strong>',
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            cancelButtonText: '<div style = "font-family:Kanit"> ยกเลิก </div>',
+            confirmButtonText: '<div style = "font-family:Kanit"> ตกลง </div>',
+            confirmButtonColor: '#005FBC',
+            reverseButtons: true,
+        }).then((e) => {
+              if (e.isConfirmed) {
+                  this.leaveService.leaveOnline(
+                    this.leaveDays.controls.leave.value, 
+                    this.leaveDays.controls.sick.value, 
+                    this.leaveDays.controls.ordination.value,
+                    this.leaveDays.controls.maternity.value,
+                    this.leaveDays.controls.militaryService.value, 
+                    this.leaveDays.controls.v_without_pay.value, 
+                    this.leaveDays.controls.v_resign.value, 
+                    this.leaveDays.controls.v_take_annual0_12.value, 
+                    this.leaveDays.controls.v_take_annual13_15.value,
+                    this.leaveDays.controls.v_take_annual16_18.value,
+                    this.leaveDays.controls.v_take_annual19_21.value, 
+                    this.leaveDays.controls.v_take_annual22_24.value, 
+                    this.leaveDays.controls.v_take_annual25_27.value,
+                    this.leaveDays.controls.v_take_annual28_30.value,
+                    this.leaveDays.controls.v_take_annual31_33.value, 
+                    this.leaveDays.controls.v_take_annual34_36.value, 
+                    this.leaveDays.controls.v_take_annual37_39.value,
+                    this.leaveDays.controls.v_take_annual40_42.value,
+                    this.leaveDays.controls.v_take_annual43_45.value, 
+                    this.leaveDays.controls.v_take_annual46.value, 
+                    this.leaveDays.controls.v_without_pay.value,
+                  ).subscribe({
+                      next: (res: any) => {
+                        this.route.navigate(['../main/leave']);
+                      },
+                      error: (res: any) => {
+                      }
+                  });
+              }
+        })
+
+    }
+
+
+    inputNumberOnly(event: any): boolean {
+        const charCode = event.which ? event.which : event.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+        return true;
+    }
 
 
 
 
-  
+
 
 
 }
--- src/app/main/sub-component/leave/leave.service.ts
+++ src/app/main/sub-component/leave/leave.service.ts
@@ -9,7 +9,7 @@ export class LeaveService {
 
     constructor(private httpClient: HttpClient) { }
 
-    getVerify(rvac_type:any,rvac_date_start:any,rvac_date_end:any,rvac_duration:any,) {
+    getVerify(rvac_type: any, rvac_date_start: any, rvac_date_end: any, rvac_duration: any,) {
         const headers = new HttpHeaders({
             Authorization: 'Bearer' + localStorage.getItem('tokenLocal'),
         });
@@ -28,7 +28,86 @@ export class LeaveService {
         );
     }
 
+    getleaveOnlineEdit() {
+        const headers = new HttpHeaders({
+            Authorization: 'Bearer' + localStorage.getItem('tokenLocal'),
+        });
+
+        return this.httpClient.get(
+            `${environment.apiURL}/leaveOnline/edit`,
+            {
+                headers,
+            }
+        );
+    }
+
+    leaveOnline(
+        v_leave: any,
+        v_sick: any,
+        v_ordination: any,
+        v_maternity: any,
+        v_sterilization: any,
+        v_military_service: any,
+        v_without_pay: any,
+        v_resign: any,
+        v_take_annual0_12: any,
+        v_take_annual13_15: any,
+        v_take_annual16_18: any,
+        v_take_annual19_21: any,
+        v_take_annual22_24: any,
+        v_take_annual25_27: any,
+        v_take_annual28_30: any,
+        v_take_annual31_33: any,
+        v_take_annual34_36: any,
+        v_take_annual37_39: any,
+        v_take_annual40_42: any,
+        v_take_annual43_45: any,
+        v_take_annual46: any
+    ) {
+        const headers = new HttpHeaders({
+            Authorization: 'Bearer' + localStorage.getItem('tokenLocal'),
+        });
+
+        return this.httpClient.put(
+            `${environment.apiURL}/leaveOnline/edit`,
+            {
+                "v_leave": v_leave,
+                "v_sick": v_sick,
+                "v_ordination": v_ordination,
+                "v_maternity": v_maternity,
+                "v_sterilization": v_sterilization,
+                "v_military_service": v_military_service,
+                "v_without_pay": v_without_pay,
+                "v_resign": v_resign,
+                "v_take_annual0_12": v_take_annual0_12,
+                "v_take_annual13_15": v_take_annual13_15,
+                "v_take_annual16_18": v_take_annual16_18,
+                "v_take_annual19_21": v_take_annual19_21,
+                "v_take_annual22_24": v_take_annual22_24,
+                "v_take_annual25_27": v_take_annual25_27,
+                "v_take_annual28_30": v_take_annual28_30,
+                "v_take_annual31_33": v_take_annual31_33,
+                "v_take_annual34_36": v_take_annual34_36,
+                "v_take_annual37_39": v_take_annual37_39,
+                "v_take_annual40_42": v_take_annual40_42,
+                "v_take_annual43_45": v_take_annual43_45,
+                "v_take_annual46": v_take_annual46
+            },
+            {
+                headers
+            }
+        );
+    }
+
 }
+
+
+
+
+// leaveOnline/edit [get]
+// ดูแก้ไขวันลาประจำปี ตัวแปรตามภาพ
+// leaveOnline [put]
+// แก้ไขวันลาประจำปี ส่งตัวแปรชื่อเดิมตามภาพ
 
 // /leaveOnline/checkDate[GET]
 //                 'rvac_type'  :  1,
